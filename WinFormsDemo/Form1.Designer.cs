92562b0f761fdc62bb144b50dbbc3241b947ff00
xiaoyunwei <wisecloud@hotmail.com>
2017-09-12 03:01:22 +0800

    添加演示客户端

---
+++ WinFormsDemo/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WinFormsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.tmrCheckMessage = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "用户名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "密码：";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(88, 33);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(125, 21);
+            this.txtName.TabIndex = 2;
+            this.txtName.Text = "tom";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(88, 61);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(125, 21);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.Text = "abcd1234";
+            // 
+            // tmrCheckMessage
+            // 
+            this.tmrCheckMessage.Interval = 3000;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(88, 109);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "登陆";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Timer tmrCheckMessage;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ WinFormsDemo/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using RestSharp.Portable;
+using RestSharp.Portable.HttpClient;
+
+namespace WinFormsDemo
+{
+    public partial class Form1 : Form
+    {
+        string baseUrl;
+        IRestClient restClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            baseUrl = ConfigurationManager.AppSettings["ChatWebBaseUrl"];
+            restClient = new RestClient(baseUrl);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            RestRequest request = new RestRequest("home/login", Method.POST);
+            LoginViewModel model = new LoginViewModel();
+            model.UserName = txtName.Text.Trim();
+            model.Password = txtPassword.Text.Trim();
+            request.AddBody(model);
+            //request.AddParameter("model", model, ParameterType.RequestBody);
+            var task = restClient.Execute<Object>(request);
+            task.Wait();
+            if(task.Result.Data==null)
+            {
+                MessageBox.Show("ok");
+            }
+        }
+    }
+
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    public class ChatMessage
+    {
+        public string SenderName { get; set; }
+
+        public string ReceiverId { get; set; }
+
+        public string Message { get; set; }
+
+        public bool IsOffline { get; set; }
+
+        public DateTime SendTime { get; set; }
+
+        public DateTime? ReceiveTime { get; set; }
+    }
+}
